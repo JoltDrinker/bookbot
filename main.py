a74693f7c34a3d2a09911433b23055a4ff36f1e5
Dustin Cade <cade.dustint@gmail.com>
2024-10-20 18:29:39 -0500

    Added character count

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     words = len(file_contents.split())
-    print(file_contents)
+
+    lowered_string = file_contents.lower()
+    char_count = {}
+    for char in lowered_string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
+
+    #print(file_contents)
     print("Word count = ", words)
